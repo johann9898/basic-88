6b6b409b2cfca0576782c405e4c326287cc4d9a5
Jóhann Rúnarsson <johann9898@gmail.com>
2016-04-05 13:30:32 +0000

    Vef_2a

--- index.php
+++ index.php
@@ -1,64 +1,50 @@
+<?php include 'include/intro.php' ?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head>
-<title>Welcome <?php include 'php/title.php';?></title>
+<title>Welcome <?php include 'include/title.php';?></title>
 <meta charset="iso-8859-1">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
 <link rel="stylesheet" href="styles/layout.css" type="text/css">
+<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
+<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/grids-responsive-min.css">
+<link rel="stylesheet" href="css/font-awesome.min.css">
 <!--[if lt IE 9]><script src="scripts/html5shiv.js"></script><![endif]-->
 </head>
 <body>
-<div class="wrapper row1">
+<div>
+  
   <header id="header" class="clear">
     <div id="hgroup">
-      <h1><a href="#">Layout</a></h1>
+      <h1 class="font_move">Home</h1>
       </div>
-    <nav>
-      <?php include 'php/menu.php';?>
 
+    <nav>
+    <div class="pure-menu pure-menu-open pure-menu-horizontal" >
+      <?php include 'include/menu.php';?>
+      </div>
     </nav>
   </header>
 </div>
 <!-- content -->
-<div class="wrapper row2">
-  <div id="container" class="clear">
+<div  clsss"pure-g" id="middle">
+<section>
+  <div id="container" class="pure-u-1 pure-u-sm-24-24 pure-u-md-24-24 pure-u-lg-24-24">
     <!-- Slider -->
-    <section id="slider"><a href="#"><?php echo '<img src="images/demo/960x360.gif"  alt="">' ?> </a></section>
+    <section class="move"><img src= "<?php echo $input[array_rand($input)]; ?>" alt=""></section>
     <!-- main content -->
-    <div id="homepage">
-      <!-- Services -->
-      <section id="services" class="clear">
-        <article class="one_third">
-          <figure><?php echo '<img src="images/demo/290x180.gif" width="290" height="180" alt="">' ?>
-           
-          </figure>
-        </article>
-        <article class="one_third">
-          <figure><?php echo '<img src="images/demo/290x180.gif" width="290" height="180" alt="">' ?>
-            
-          </figure>
-        </article>
-        <article class="one_third lastbox">
-          <figure><?php echo '<img src="images/demo/290x180.gif" width="290" height="180" alt="">' ?>
-           
-          </figure>
-        </article>
-      </section>
-      <!-- / Services -->
-      <!-- ########################################################################################## -->
-      <!-- ########################################################################################## -->
-      <!-- ########################################################################################## -->
-      <!-- ########################################################################################## -->
-      <!-- Introduction -->
-      
-      <!-- / Introduction -->
-    </div>
+    
     <!-- / content body -->
   </div>
+  </section>
 </div>
 
 </div>
 <footer  id="copyright" class="clear">
-  <p>&copy; <?php $startYear = 2016; $thisYear = date('Y'); if ($startYear == $thisYear) {    echo $startYear; } else {    echo "{$startYear}&ndash;{$thisYear}"; } ?> Jóhann Rúnarsson</p>
+  <p>&copy; <?php include 'include/footer.php'?>  Jóhann Rúnarsson</p>
 </footer>
 </body>
 </html>
